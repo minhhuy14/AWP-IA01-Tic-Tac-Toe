cb3dad1895869be534a0d60ff9b7202d6e9baabb
minhhuy14 <minhhuy14012003@gmail.com>
2024-10-09 14:53:37 +0700

    Complete requirement 04: Display the location for each move in the format (row, col)

--- src/App.js
+++ src/App.js
@@ -8,12 +8,16 @@ function Square({ value, onSquareClick }) {
   );
 }
 
-function Board({ xIsNext, squares, onPlay, sizeBoard }) {
+function Board({ xIsNext, squares, onPlay, sizeBoard, setLocations }) {
 
   function handleClick(i) {
     if (calculateWinner(squares) || squares[i]) {
       return;
-    }
+    };
+
+    let location = convertTo2DCoordinate(i, sizeBoard);
+    setLocations(current => [...current, location]);
+
     const nextSquares = squares.slice();
     if (xIsNext) {
       nextSquares[i] = 'X';
@@ -31,18 +35,26 @@ function Board({ xIsNext, squares, onPlay, sizeBoard }) {
     status = 'Next player: ' + (xIsNext ? 'X' : 'O');
   }
 
-  const squareRows = useState(Array(sizeBoard).fill(null));
+  const squareRows = Array(sizeBoard).fill(null);
+
+  function convertTo2DCoordinate(value, sizeBoard) {
+    return [
+      Math.floor(value / sizeBoard),
+      value % sizeBoard
+    ]
+
+  }
 
   return (
     <>
       <div className="status">{status}</div>
       {
         squareRows.map((row, rowIndex) => {
-          return <div className="board-row">
+          return <div key={rowIndex} className="board-row">
             {
               squareRows.map((col, colIndex) => {
                 const squareIndex = rowIndex * sizeBoard + colIndex;
-                return <Square value={squares[squareIndex]}
+                return <Square value={squares[squareIndex]} key={squareIndex}
                   onSquareClick={() => handleClick(squareIndex)} />
               })
             }
@@ -55,39 +67,57 @@ function Board({ xIsNext, squares, onPlay, sizeBoard }) {
 }
 
 export default function Game() {
+
+  const [sizeBoard, setSizeBoard] = useState(3);
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
   const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
 
+  const [locations, setLocations] = useState([]);
+
   function handlePlay(nextSquares) {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
     setHistory(nextHistory);
     setCurrentMove(nextHistory.length - 1);
+    console.log(locations);
+
   }
 
   function jumpTo(nextMove) {
     setCurrentMove(nextMove);
   }
 
+
   const moves = history.map((squares, move) => {
     let description;
-    if (move > 0) {
-      description = 'Go to move #' + move;
-    } else {
+    if (move == 0) {
       description = 'Go to game start';
     }
+    else if (move > 0 && move == currentMove) {
+      description = `You are at move #${move}: (${locations[move - 1]})`;
+    }
+    else if (move > 0) {
+      description = `Go to move #${move}: (${locations[move - 1]})`;
+    }
+
+
     return (
       <li key={move}>
-        <button onClick={() => jumpTo(move)}>{description}</button>
+        {move === currentMove ? (
+          <span>{description}</span>
+        ) : (
+          <button onClick={() => jumpTo(move)}>{description}</button>
+        )}
       </li>
-    );
+    )
   });
+
 
   return (
     <div className="game">
       <div className="game-board">
-        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} sizeBoard={3} />
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} sizeBoard={sizeBoard} setLocations={setLocations} />
       </div>
       <div className="game-info">
         <ol>{moves}</ol>
@@ -114,4 +144,6 @@ function calculateWinner(squares) {
     }
   }
   return null;
+
+
 }
