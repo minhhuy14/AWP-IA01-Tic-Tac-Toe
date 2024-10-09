857320fd90168f1c7f43074d8a5d6e010299a640
minhhuy14 <minhhuy14012003@gmail.com>
2024-10-09 08:51:01 +0700

    Complete requirement 02: use two loops to make squares

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function Square({ value, onSquareClick }) {
   );
 }
 
-function Board({ xIsNext, squares, onPlay }) {
+function Board({ xIsNext, squares, onPlay, sizeBoard }) {
+
   function handleClick(i) {
     if (calculateWinner(squares) || squares[i]) {
       return;
@@ -30,24 +31,25 @@ function Board({ xIsNext, squares, onPlay }) {
     status = 'Next player: ' + (xIsNext ? 'X' : 'O');
   }
 
+  const squareRows = useState(Array(sizeBoard).fill(null));
+
   return (
     <>
       <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+      {
+        squareRows.map((row, rowIndex) => {
+          return <div className="board-row">
+            {
+              squareRows.map((col, colIndex) => {
+                const squareIndex = rowIndex * sizeBoard + colIndex;
+                return <Square value={squares[squareIndex]}
+                  onSquareClick={() => handleClick(squareIndex)} />
+              })
+            }
+          </div>
+        })
+      }
+
     </>
   );
 }
@@ -85,7 +87,7 @@ export default function Game() {
   return (
     <div className="game">
       <div className="game-board">
-        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} sizeBoard={3} />
       </div>
       <div className="game-info">
         <ol>{moves}</ol>
